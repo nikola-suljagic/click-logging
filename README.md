fb20417569db4e88d2852dea2da298bf1200ec85
nsuljagic <nsuljagic@divstechnology.com>
2023-12-07 08:23:57 +0100

    Logging tool v1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # click-logging
-Python tooling to handle log messages that are sent from mikroE click board
+Python tool to handle log messages that are sent from mikroE development board
+
+
+## Project setup
+
+* Install poetry python package manager
+    - Ubuntu
+        * Installation: `curl -sSL https://install.python-poetry.org | python3 -`
+        * Verify installation: poetry --version
+
+* Install python environment via poetry: `poetry install`
+* Change permission for serial port: `sudo chmod 777 /dev/ttyUSB0`
+
+
+## Logging
+
+* Run logging: `poetry run python click_logging.py`
+* Run logging with log file argument: `poetry run python click_logging.py --log-file "log_events.csv"`
+* Stop logging by pressing `CTRL + C`
+* Log artifacts should appear in **events.csv** if --log-file isn't specified or in log file that is specified via --log-file arg 
---
+++ click_logging.py
@@ -0,0 +1,46 @@
+from serial import Serial
+import ast
+import datetime
+import pandas as pd
+import logging
+from argparse import ArgumentParser
+
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+                    prog='ClickLogging',
+                    description='Python tool for catching event messages from mikroE development board')
+    
+    
+    parser.add_argument("--log-file", type=str, default="events.csv", help="Specify file where events will be stored in .csv format")
+    
+    args = parser.parse_args()
+    
+    
+    ser = Serial(
+        "/dev/ttyUSB0",
+        timeout=None,
+        baudrate=921600,
+        xonxoff=False,
+        rtscts=False,
+        dsrdtr=False,
+    )
+
+    events = []
+    try:
+        while True:
+            data = ser.readline()
+            try:
+                data_str = data.decode("utf-8")
+                event_data = ast.literal_eval(data_str)
+                event_data["time"] = str(datetime.datetime.now())
+                events.append(event_data)
+                logging.info(msg=f"[EVENT] {event_data}")
+            except ValueError:
+                logging.error("Can't create dict from event")
+    except KeyboardInterrupt as ki:
+        events_df = pd.DataFrame(events)
+
+        events_df.to_csv(args.log_file, index=False)
